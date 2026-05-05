c5af994b0272de6d7288febcea080c8be3e36228
GDTran15 <trangiaiduong1512@gmail.com>
2026-05-05 22:06:25 +1000

    add item price to get order item price

--- backend/restaurant-management/src/main/java/com/duong/RestaurantManagement/dto/order/response/GetOrderItemDTO.java
+++ backend/restaurant-management/src/main/java/com/duong/RestaurantManagement/dto/order/response/GetOrderItemDTO.java
@@ -3,6 +3,7 @@ package com.duong.RestaurantManagement.dto.order.response;
 public record GetOrderItemDTO(
         String foodName,
         int quantity,
+        double itemPrice,
         double foodPrice
 ) {
 }
